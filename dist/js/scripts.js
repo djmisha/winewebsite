30e06f0c20a7fa2b93a27eaf316f0dedb43acdc5
Misha Osinvoskiy <misha.osinovskiy@gmail.com>
2021-02-22 08:59:02 -0800

    daily commit

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,10 +1,10 @@
 /*! sethmckinney.com v0.0.1 | (c) 2021 Misha | MIT License | https://github.com/djmisha/sethmckinney.com */
 // IFFE
-(function () {
-    var face = document.querySelector(".face");
-    var body = document.querySelector("body");
-    face.addEventListener("click", (function (event) {
-        event.target.classList.toggle("spin");
-        body.classList.toggle("spin");
-    }));
-})();
+// (function () {
+//     var face = document.querySelector(".face");
+//     var body = document.querySelector("body");
+//     face.addEventListener("click", function (event) {
+//         event.target.classList.toggle("spin");
+//         body.classList.toggle("spin");
+//     });
+// })();
